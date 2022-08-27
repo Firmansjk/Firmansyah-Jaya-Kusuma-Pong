0ec9c1aa262864020c4e860ead882e5f11198f7e
Firmansjk <88803824+Firmansjk@users.noreply.github.com>
2022-08-27 09:26:36 +0800

    Adding UI & Game Manager

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -21,6 +21,6 @@ public class BallController : MonoBehaviour
 
     public void ResetBall()
     {
-        transform.position = new Vector3(resetPosition.x, resetPosition.y, 2);
+        transform.position = new Vector3(resetPosition.x, resetPosition.y, 1);
     }
 }
