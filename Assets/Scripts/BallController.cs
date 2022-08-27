a81d349293cc85ad961879d9cf68b68294dc8457
Firmansjk <88803824+Firmansjk@users.noreply.github.com>
2022-08-27 08:50:22 +0800

    Adding UI & Game Manager
    menambahkan UI sederhana dan membuat sistem scoring

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -6,16 +6,21 @@ public class BallController : MonoBehaviour
 {
     public Vector2 speed;
     private Rigidbody2D rig;
-    // Start is called before the first frame update
+    public Vector2 resetPosition;
+
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
         rig.velocity = speed;
     }
 
-    // Update is called once per frame
     void Update()
     {
         //transform.Translate(speed * Time.deltaTime);
+    }
+
+    public void ResetBall()
+    {
+        transform.position = new Vector3(resetPosition.x, resetPosition.y, 2);
     }
 }
---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("Created by Firman");
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void OpenAuthor()
+    {
+        Debug.Log("Created by Firman");
+    }
+}
